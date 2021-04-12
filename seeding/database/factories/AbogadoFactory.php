f2bab6a9ee97484857b22210db5ecbf5fd691593
diegopa <diego.loge@hotmail.com>
2021-04-12 13:50:10 -0500

    seeder abogado

---
+++ seeding/database/factories/AbogadoFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Abogado;
+use Faker\Generator as Faker;
+
+$factory->define(Abogado::class, function (Faker $faker) {
+    return [
+        'nombre' => $faker->firstName,
+        'apellido' => $faker->lastName,
+        'fecha_registro' => $faker->date($format = 'Y-m-d', $max = 'now'),
+    ];
+});
+
---
+++ seeding/database/factories/PrioridadFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Prioridad;
+use Faker\Generator as Faker;
+
+$factory->define(Prioridad::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
